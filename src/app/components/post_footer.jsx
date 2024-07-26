a22ac8e758c8243c67d0ff4f58cc1092bb123880
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-07-27 00:31:00 +0900

    もう一回

--- src/app/components/post_footer.jsx
+++ src/app/components/post_footer.jsx
@@ -1,7 +1,7 @@
 "use client"; // クライアント側で動作するコードであることを指定しています。
 import React, { useState } from "react";
 import { Button } from "@mui/material"; // Material-UIのボタンコンポーネントをインポートしています。
-import UploadImageModal from "@/components/modal"; // モーダルコンポーネントをインポートしています。
+import UploadImageModal from "@/components/Modal"; // モーダルコンポーネントをインポートしています。
 import Link from "next/link"; // ページ間リンクを作成するためのコンポーネントをインポートしています。
 
 export default function Post_Footer({ theme }) {
@@ -27,10 +27,11 @@ export default function Post_Footer({ theme }) {
     <div
       style={{
         position: "fixed",
-        bottom: 30,
+        bottom: 0,
         width: "100%",
         zIndex: 1000,
         backgroundColor: theme === "light" ? "#fff" : "#333",
+        color: theme === "light" ? "#000" : "#fff",
       }}
     >
       <div>
