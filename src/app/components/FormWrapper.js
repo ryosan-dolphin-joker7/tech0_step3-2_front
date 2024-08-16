fc7083880cb5dd4c5dbc0bfd6da1c0e349581d6c
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-08-16 23:44:43 +0900

    inputoとbuttonで標準のコンポーネントを使うように修正

--- src/app/components/FormWrapper.js
+++ src/app/components/FormWrapper.js
@@ -3,18 +3,13 @@ import { useForm } from "react-hook-form";
 import { signIn } from "next-auth/react";
 
 export default function FormWrapper() {
-  // フォーム関連の変数定義
-  const { control, handleSubmit } =
-    useForm <
-    InputProps >
-    {
-      defaultValues: {
-        email: "",
-        password: "",
-      },
-    };
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-  // フォーム送信処理
   const onSubmit = async (data) => {
     const result = await signIn("user", {
       redirect: false,
@@ -23,7 +18,7 @@ export default function FormWrapper() {
     });
 
     if (result?.error) {
-      // ログイン失敗時処理
+      // ログイン失敗時の処理
     } else {
       // ログイン成功時トップページへリダイレクト
       location.href = "/";
@@ -31,35 +26,38 @@ export default function FormWrapper() {
   };
 
   return (
-    <>
-      <form
-        className="max-w-[450px] w-full mx-auto border rounded-xl p-4 shadow-md"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="mb-5">
-          <div className="font-bold mb-2">メールアドレス</div>
-          <div className="flex items-start gap-8">
-            <InputControl name="email" type="email" control={control} />
-          </div>
-        </div>
+    <form
+      className="max-w-[450px] w-full mx-auto border rounded-xl p-4 shadow-md"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="mb-5">
+        <label className="font-bold mb-2">メールアドレス</label>
+        <input
+          {...register("email")}
+          type="email"
+          className="border p-2 w-full"
+          placeholder="メールアドレス"
+        />
+      </div>
 
-        <div className="mb-5">
-          <div className="font-bold mb-2">パスワード</div>
-          <div className="flex items-start gap-8">
-            <InputControl name="password" type="password" control={control} />
-          </div>
-        </div>
+      <div className="mb-5">
+        <label className="font-bold mb-2">パスワード</label>
+        <input
+          {...register("password")}
+          type="password"
+          className="border p-2 w-full"
+          placeholder="パスワード"
+        />
+      </div>
 
-        <div className="flex justify-center">
-          <GreenButton
-            element="button"
-            className="max-w-[250px] py-3 text-sm"
-            type="submit"
-          >
-            ログイン
-          </GreenButton>
-        </div>
-      </form>
-    </>
+      <div className="flex justify-center">
+        <button
+          className="max-w-[250px] py-3 text-sm bg-blue-500 text-white rounded-lg"
+          type="submit"
+        >
+          ログイン
+        </button>
+      </div>
+    </form>
   );
 }
