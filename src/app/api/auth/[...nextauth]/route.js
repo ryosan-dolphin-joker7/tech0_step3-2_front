9a1317c1a96a57298b7b695d753fa4f84d8baeb0
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-08-18 21:18:41 +0900

    ログイン認証機能の実装を開始

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,4 +3,8 @@ import { authOptions } from "@/lib/next-auth/authOptions";
 
 const handler = NextAuth(authOptions);
 
+console.log("API Route Accessed");
+console.log("NextAuth API Route is being accessed");
+console.log("API Route Hit: /api/auth/[...nextauth]");
+
 export { handler as GET, handler as POST };
