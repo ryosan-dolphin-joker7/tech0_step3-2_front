750eae2b8c9554af1b2794f715882a2f7b7f6a0a
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-08-21 20:05:12 +0900

    ログイン画面にメッセージを追加しました

--- src/app/components/LoginPrompt.jsx
+++ src/app/components/LoginPrompt.jsx
@@ -5,6 +5,13 @@ export default function LoginPrompt() {
     <div className="flex items-center justify-center min-h-screen">
       <div className="text-center">
         <h1 className="text-2xl font-bold mb-4">ログインが必要です</h1>
+        <p className="mb-4">メールアドレスは「test1@wanpush.com」です。</p>
+        <p className="mb-4">
+          パスワードは発表会場のアルファベット6文字です「k〇k〇yo」
+        </p>
+        <p className="mb-4">
+          入力画面から戻るときはブラウザの戻るを使ってください
+        </p>
         <button
           onClick={() => signIn()}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
